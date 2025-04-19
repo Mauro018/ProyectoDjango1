b868fcbb2efbace9491b475c1a63eaf8911f969d
Mauro017 <carlosmauricioarevalozapata13@gmail.com>
2025-04-19 18:59:26 -0500

    m

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Tarea
 
+# Create your views here.
+def index(request):
+    tareas = Tarea.objects.all()
+    context = {'tareas':tareas}
+    return render(request, 'Todo/index.html',context)
 

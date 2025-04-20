3c156792530705ec82b0df55dbc591ff337421f7
Mauro017 <carlosmauricioarevalozapata13@gmail.com>
2025-04-19 20:33:10 -0500

    Mejoras

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-from .models import Tarea
+from django.shortcuts import redirect, render
 
+from todo.forms import TareaForm
+from .models import Tarea
 # Create your views here.
 def index(request):
     tareas = Tarea.objects.all()
     context = {'tareas':tareas}
-    return render(request, 'Todo/index.html',context)
+    return render(request, 'todo/index.html',context)
 
+def agregar(request):
+    if request.method == "POST":
+        form = TareaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = TareaForm()
+    context = {'form':form}
+    return render(request,'todo/agregar.html',context)
